0c85cedd3a1176cc4acac5539d989cecb7f2caa0
Dev JP <jpjonker.business@gmail.com>
2022-04-20 10:46:40 +0200

    added instant api call on page load with js

--- js/generate-advice.js
+++ js/generate-advice.js
@@ -1,6 +1,10 @@
-$("#generate-dice").click(() => {
+function getApiData() {
   $.getJSON("https://api.adviceslip.com/advice", function (data) {
     $("#advice-id-value").text(data.slip.id);
     $("#advice-text").text(data.slip.advice);
   });
-});
+}
+
+$("#generate-dice").click(getApiData);
+
+getApiData();
